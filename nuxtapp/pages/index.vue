930b2107d62004b41402e4b45160b9a2f42e81cb
719yen <r_f.sakura@icloud.com>
2022-05-15 01:56:35 +0900

    環境設定用

---
+++ nuxtapp/pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  <h1>{{message}}</h1>
+  <b-button v-on:click="openExploler"  variant="outline-info">ファイルを開く</b-button>
+
+</div>
+</template>
+<style>
+h1{
+  font-size: 19px;
+}
+</style>
+<script>
+import BootstrapVue from 'bootstrap-vue'
+export default {
+  data() {
+    return {
+      message: 'hello'
+    };
+  },
+  methods:{
+   openExploler: function(){
+      window.open("C:\Users\r_fsa");
+   } 
+
+  }
+
+}
+</script>

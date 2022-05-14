bd78eed7f5c324bbd9f78eaabfc6b66b630f02fd
Teppei Kamiike <nohe.18tro@gmail.com>
2022-05-15 02:40:00 +0900

    test

--- nuxtapp/pages/index.vue
+++ nuxtapp/pages/index.vue
@@ -1,29 +1,28 @@
 <template>
-<div>
-  <h1>{{message}}</h1>
-  <b-button v-on:click="openExploler"  variant="outline-info">ファイルを開く</b-button>
-
-</div>
+  <div>
+    <h1>{{ message }}</h1>
+    <b-button v-on:click="openExploler" variant="outline-info"
+      >ファイルを開く</b-button
+    >
+  </div>
 </template>
 <style>
-h1{
+h1 {
   font-size: 19px;
 }
 </style>
 <script>
-import BootstrapVue from 'bootstrap-vue'
+import BootstrapVue from "bootstrap-vue";
 export default {
   data() {
     return {
-      message: 'hello'
+      message: "hello tatu",
     };
   },
-  methods:{
-   openExploler: function(){
+  methods: {
+    openExploler: function () {
       window.open("C:\Users\r_fsa");
-   } 
-
-  }
-
-}
+    },
+  },
+};
 </script>
